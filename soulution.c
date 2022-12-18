5b4724c69f7ffe9f24f5b11b39ec6a2ddad65d3a
parkseongsoo <102282941+parkseongsooo@users.noreply.github.com>
2022-12-18 14:19:20 +0900

    기사단원의 무기(Lv.1)

--- soulution.c
+++ soulution.c
@@ -2,8 +2,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int n) {
-    for(int i=1;i<=n; i++)
-        if(n%i==1)
-            return i;          
+int solution(int number, int limit, int power) {
+    int result = 0;
+    for(int i=1; i<=number; i++){
+        int count =0;
+        for(int j =1;j<=i;j++)
+            if(i%j==0) 
+                count++;        
+        if(count<=limit)
+            result += count;
+        else
+            result += power;
+    }
+    return result;
 }
